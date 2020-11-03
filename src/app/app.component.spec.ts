8278e14c7c9797bf364ece3fb97ab4300f8cc755
yazan alkhateeb <yazan@beamwallet.me>
2020-11-03 17:20:06 +0200

    change the name of project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe("AppComponent", () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'black-dashboard-angular'`, () => {
+  it(`should have as title 'gym-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("black-dashboard-angular");
+    expect(app.title).toEqual("gym-web-angular");
   });
 
   it("should render title in a h1 tag", () => {
@@ -27,7 +27,7 @@ describe("AppComponent", () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector("h1").textContent).toContain(
-      "Welcome to black-dashboard-angular!"
+      "Welcome to gym-web-angular!"
     );
   });
 });
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/black-dashboard-angular'),
+      dir: require('path').join(__dirname, '../coverage/gym-web-angular'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
